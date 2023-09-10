3008a5dea34fe506bbc6b3653e9e2ecf9a79682d
ArthurViniciusSL <76112437+ArthurViniciusL@users.noreply.github.com>
2023-09-10 19:06:15 -0300

    Correção de bug no bnt-cv

--- test/001/script.js
+++
@@ -1,8 +0,0 @@
-const helloline = document.getElementById("helloline");
-
-let pathLength = helloline.getTotalLength();
-// console.log(pathLength)
-helloline.setAttribute("stroke-dasharray", pathLength);
-helloline.setAttribute("stroke-dashoffset", pathLength);
-console.log(helloline.innerHTML)
-helloline.innerHTML = `<animate attributeName="stroke-dashoffset" begin='1s' dur="5s" to="0" fill="freeze" />`;
