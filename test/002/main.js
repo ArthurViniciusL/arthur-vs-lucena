af21e7f73f33e9d9920039700d6f27c749d1d863
ArthurViniciusSL <76112437+ArthurViniciusL@users.noreply.github.com>
2023-09-10 14:52:58 -0300

    Finalizando testes

--- test/002/main.js
+++ test/002/main.js
@@ -1,6 +1,22 @@
-// Função para verificar o modo de cor e atualizar a página
-function verificarModoDeCor() {
+// Função assíncrona para carregar o arquivo JSON de cores
+async function carregarCores() {
+    try {
+        const JSON_FILE = await fetch('color_palette.json');
 
+        if (!JSON_FILE.ok) {
+            throw new Error('Erro ao carregar o arquivo JSON de cores.');
+        }
+
+        return await JSON_FILE.json();
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+// Função para verificar o modo de cor e atualizar a página
+async function verificarModoDeCor() {
+    /*
     const cores = {
         lightBackgrondColor: "#F5F5F7",
         lightSectionColor: "#FFFFFF",
@@ -10,10 +26,12 @@ function verificarModoDeCor() {
         darktSectionColor: "#303030",
         darkTextColor: "#FFFFFF"
     };
+    */
+    const cores = await carregarCores();
 
     const titulo = document.querySelector('#texto_titulo');
     const paragrafo = document.querySelector('#texto_paragrafo');
-    const mySection = document.querySelector('.my-section');
+    const mySection = document.getElementsByTagName('section');
     const myBtn = document.querySelector(".my-btn");
 
     const lightTheme = window.matchMedia('(prefers-color-scheme: light)');
@@ -30,26 +48,36 @@ function verificarModoDeCor() {
 
         //Mudando cores
         document.body.style.backgroundColor = cores.darkBackgrondColor;
-        mySection.style.backgroundColor = cores.darktSectionColor;
+        // mySection.style.backgroundColor = cores.darktSectionColor;
+        for (let i = 0; i < mySection.length; i++) {
+            mySection[i].style.backgroundColor = cores.darktSectionColor; // Define a cor de fundo para azul para cada elemento
+        }
+
         titulo.style.color = cores.darkTextColor;
         paragrafo.style.color = cores.darkTextColor;
 
-        myBtn.style.color = '#7243FA';
-        myBtn.style.borderColor = '#7243FA';
-        //myBtn.style.backgroundColor = '#FFFFFF';
-        myBtn.style.backgroundColor = cores.darktSectionColor;
 
 
         // Define o estilo para o hover no modo escuro
-        myBtn.addEventListener('mouseenter', () => { myBtn.style.backgroundColor = 'blue'; });
+        myBtn.addEventListener('mouseenter', () => {
+            myBtn.style.backgroundColor = 'blue';
+            myBtn.style.color = '#7243FA';
+            myBtn.style.borderColor = '#7243FA';
+            //myBtn.style.backgroundColor = '#FFFFFF';
+            //myBtn.style.backgroundColor = cores.darktSectionColor;
+        });
+
 
         // Define o estilo para quando o mouse sai no modo escuro
-        myBtn.addEventListener('mouseleave', () => { myBtn.style.backgroundColor = 'red'; });
+        myBtn.addEventListener('mouseleave', () => {
+            myBtn.style.backgroundColor = 'red';
+            myBtn.style.color = '#7243FA';
+            myBtn.style.borderColor = '#7243FA';
+            //myBtn.style.backgroundColor = '#FFFFFF';
+            //myBtn.style.backgroundColor = cores.darktSectionColor;
+        });
 
-
-        document.body.style.transition = '0.5s ease';
-        paragrafo.style.transition = '1s ease';
-
+        document.body.style.transition = 'background-color 0.5s ease, color 0.5s ease';
 
     } else {
         browserTheme = "LIGHT";
@@ -60,7 +88,11 @@ function verificarModoDeCor() {
 
         //Mudando cores
         document.body.style.backgroundColor = cores.lightBackgrondColor;
-        mySection.style.backgroundColor = cores.lightSectionColor;
+        // mySection.style.backgroundColor = cores.lightSectionColor;
+
+        for (let i = 0; i < mySection.length; i++) {
+            mySection[i].style.backgroundColor = cores.lightSectionColor; // Define a cor de fundo para azul para cada elemento
+        }
         titulo.style.color = cores.lightTextColor;
         paragrafo.style.color = cores.lightTextColor;
 
@@ -69,14 +101,25 @@ function verificarModoDeCor() {
         myBtn.style.backgroundColor = '';
 
         // Define o estilo para o hover no modo escuro
-        myBtn.addEventListener('mouseenter', () => { myBtn.style.backgroundColor = ''; });
+        myBtn.addEventListener('mouseenter', () => {
+            myBtn.style.backgroundColor = '';
+            myBtn.style.backgroundColor = '';
+            myBtn.style.backgroundColor = '';
+            myBtn.style.color = '';
+            myBtn.style.borderColor = '';
+
+        });
 
         // Define o estilo para quando o mouse sai no modo escuro
-        myBtn.addEventListener('mouseleave', () => { myBtn.style.backgroundColor = ''; });
+        myBtn.addEventListener('mouseleave', () => {
+            myBtn.style.backgroundColor = '';
+            myBtn.style.backgroundColor = '';
+            myBtn.style.color = '';
+            myBtn.style.borderColor = '';
+            ;
+        });
 
-        document.body.style.transition = '0.5s ease';
-        paragrafo.style.transition = '1s ease';
-
+        document.body.style.transition = 'background-color 0.5s ease, color 0.5s ease';
     }
 }
 
