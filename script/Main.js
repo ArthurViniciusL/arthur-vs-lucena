094bc416eebabcd223d19d55bfc01a1d65869c7f
ArthurViniciusSL <76112437+ArthurViniciusL@users.noreply.github.com>
2023-09-10 16:20:28 -0300

    JSON para estilos

--- script/Main.js
+++ script/Main.js
@@ -1,9 +1,9 @@
 async function readJson() {
     try {
-        const JSON_FILE = await fetch('color_palette.json');
+        const JSON_FILE = await fetch('script/style_attributes.json');
 
         if (!JSON_FILE.ok) {
-            throw new Error('Erro ao carregar o arquivo JSON de cores.');
+            throw new Error('Erro ao carregar o arquivo JSON de CORES.');
         }
 
         return await JSON_FILE.json();
@@ -12,7 +12,6 @@ async function readJson() {
         return null;
     }
 }
-
 
 async function changeThemeColor() {
 
@@ -26,7 +25,8 @@ async function changeThemeColor() {
     // lendo componentes
     const TITLE_NAME = document.querySelector('#title_my_name');
     const TITLE_ABOUT = document.querySelector('#title_about');
-    const TEXT_ABOUT = document.querySelectorAll('p');
+    const TEXT_ABOUT = document.querySelector('.container-bout-text');
+
     const BTN_CV = document.querySelector('.btn-cv');
     // getElementsByTagName retorna uma lista de atributos:
     const TAG_SECTION = document.getElementsByTagName('section');
@@ -34,16 +34,17 @@ async function changeThemeColor() {
     if ((LIGHT_THEME && DARK_THEME).matches) {
         browserTheme = 'dark mode';
 
-        // alterando cores dos componentes
-        document.body.style.backgroundColor = CORES.darkBackgrondColor;
+        // alterando CORES dos componentes
+        document.body.style.backgroundColor = CORES.dark.darkBackgrondColor;
         for (let i = 0; i < TAG_SECTION.length; i++) {
             TAG_SECTION[i].style.backgroundColor = CORES.darktSectionColor;
         }
         TITLE_NAME.style.color = CORES.darkTextColor;
+        TITLE_ABOUT.style.color = CORES.darkTextColor;
         TEXT_ABOUT.style.color = CORES.darkTextColor;
 
         BTN_CV.addEventListener('mouseenter', () => {
-            BTN_CV.style.borderColor = '';
+            BTN_CV.style.borderColor = 'red';
             BTN_CV.style.color = '';
             BTN_CV.style.backgroundColor = '';
         });
@@ -55,17 +56,17 @@ async function changeThemeColor() {
         });
 
 
-
     } else {
         browserTheme = 'light mode';
 
         //zerando mudancas do dark mode
         document.body.style.backgroundColor = CORES.lightBackgrondColor;
         for (let i = 0; i < TAG_SECTION.length; i++) {
-            TAG_SECTION[i].style.backgroundColor = cores.lightSectionColor; // Define a cor de fundo para azul para cada elemento
+            TAG_SECTION[i].style.backgroundColor = CORES.lightSectionColor; // Define a cor de fundo para azul para cada elemento
         }
-        TITLE_NAME.style.color = CORES.lightextColor;
-        TEXT_ABOUT.style.color = CORES.lightTextColor;
+        TITLE_NAME.style.color = CORES.lightTextColor;
+        TITLE_ABOUT.style.color = CORES.lightTextColor
+        //TEXT_ABOUT.style.color = CORES.lightTextColor;
 
         BTN_CV.addEventListener('mouseenter', () => {
             BTN_CV.style.borderColor = '';
